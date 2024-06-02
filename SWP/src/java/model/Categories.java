81aaac98c5c4a766480a4b438b2ab0d9461c9e0a
tungtthe173042 <tungtthe25102003@gmail.com>
2024-06-02 22:44:38 +0700

    a

---
+++ SWP/src/java/model/Categories.java
@@ -0,0 +1,40 @@
+
+
+
+
+package model;
+
+
+
+import java.util.*;
+import java.lang.*;
+
+public class Categories {
+    int cid;
+    String cname;
+
+    public Categories() {
+    }
+
+    public Categories(int cid, String cname) {
+        this.cid = cid;
+        this.cname = cname;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+    
+}
